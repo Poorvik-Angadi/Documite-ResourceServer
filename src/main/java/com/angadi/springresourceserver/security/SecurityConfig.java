f7b6f699c063080ec2474410434b134434410c4d
Poorvik <poorvik.angadi@gmail.com>
2025-12-15 22:16:13 -0500

    Whitelisting urls

--- src/main/java/com/angadi/springresourceserver/security/SecurityConfig.java
+++ src/main/java/com/angadi/springresourceserver/security/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig {
     String[] whiteList = {
             "/v3/api-docs/**",
             "/swagger-ui.html",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+            "/public/**"
     };
 
 
@@ -28,7 +29,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers(whiteList).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth -> oauth
